af3be159fe4e94627f6f0fafe35c707bb5d71a6b
amit-g0swami <goswamiempire@gmail.com>
2024-03-02 01:23:22 +0530

    add: sorting algorithms

--- src/solutions.ts
+++ src/solutions.ts
@@ -6,6 +6,127 @@ interface ISolutionFunctions {
   setOutput: SetOutput;
 }
 
+const sort = (input: string | number) => {
+  const splitInput = input.toString().split("");
+  const numericInput = splitInput.map(Number);
+  return numericInput.sort((a, b) => a - b);
+};
+
+const bubbleSort = (input: string | number) => {
+  const splitInput = input.toString().split("");
+  const numericInput = splitInput.map(Number);
+  for (let i = 0; i < numericInput.length; i++) {
+    for (let j = 0; j < numericInput.length - i - 1; j++) {
+      if (numericInput[j] > numericInput[j + 1]) {
+        let temp = numericInput[j];
+        numericInput[j] = numericInput[j + 1];
+        numericInput[j + 1] = temp;
+      }
+    }
+  }
+  return numericInput;
+};
+
+const selectionSort = (input: string | number) => {
+  const splitInput = input.toString().split("");
+  const numericInput = splitInput.map(Number);
+  for (let i = 0; i < numericInput.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < numericInput.length; j++) {
+      if (numericInput[j] < numericInput[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      let temp = numericInput[i];
+      numericInput[i] = numericInput[min];
+      numericInput[min] = temp;
+    }
+  }
+  return numericInput;
+};
+
+const insertionSort = (input: string | number) => {
+  const splitInput = input.toString().split("");
+  const numericInput = splitInput.map(Number);
+  for (let i = 1; i < numericInput.length; i++) {
+    let current = numericInput[i];
+    let j = i - 1;
+    while (j >= 0 && numericInput[j] > current) {
+      numericInput[j + 1] = numericInput[j];
+      j--;
+    }
+    numericInput[j + 1] = current;
+  }
+  return numericInput;
+};
+
+const mergeSort = (input: string | number | number[]): any => {
+  const splitInput = input.toString().split("");
+  const numericInput = splitInput.map(Number);
+  if (numericInput.length <= 1) {
+    return numericInput;
+  }
+  const middle = Math.floor(numericInput.length / 2);
+  const left = numericInput.slice(0, middle);
+  const right = numericInput.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+const merge = (left: number[], right: number[]) => {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+};
+
+const quickSort = (input: string | number | number[]): any => {
+  const splitInput = input.toString().split("");
+  const numericInput = splitInput.map(Number);
+  if (numericInput.length <= 1) {
+    return numericInput;
+  }
+  const pivot = numericInput[numericInput.length - 1];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < numericInput.length - 1; i++) {
+    if (numericInput[i] < pivot) {
+      left.push(numericInput[i]);
+    } else {
+      right.push(numericInput[i]);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
+};
+
+const countingSort = (input: string | number) => {
+  const splitInput = input.toString().split("");
+  const numericInput = splitInput.map(Number);
+  const max = Math.max(...numericInput);
+  const min = Math.min(...numericInput);
+  const count = new Array(max - min + 1).fill(0);
+  for (let i = 0; i < numericInput.length; i++) {
+    count[numericInput[i] - min]++;
+  }
+  let z = 0;
+  for (let i = min; i <= max; i++) {
+    while (count[i - min]-- > 0) {
+      numericInput[z] = i;
+      z++;
+    }
+  }
+  return numericInput;
+};
+
 const containsDuplicate = ({ input, setOutput }: ISolutionFunctions) => {
   //   using set
   //   let boolean = false;
@@ -19,9 +140,7 @@ const containsDuplicate = ({ input, setOutput }: ISolutionFunctions) => {
   //     } contains duplicate`
   //   );
 
-  const splitInput = input.toString().split("");
-  const numericInput = splitInput.map(Number);
-  const sortInput = numericInput.sort((a, b) => a - b);
+  const sortInput = sort(input);
   for (let i = 0; i < sortInput.length; i++) {
     if (sortInput[i] === sortInput[i + 1]) {
       return setOutput("Yes, It does contains duplicate");
@@ -35,7 +154,18 @@ const validAnagram = ({ input, setOutput }: ISolutionFunctions) => {
   setOutput("Valid Anagram");
 };
 
+const sortingAlgorithms = {
+  sort,
+  bubbleSort,
+  selectionSort,
+  insertionSort,
+  mergeSort,
+  quickSort,
+  countingSort,
+};
+
 export const solutions = {
+  sortingAlgorithms,
   containsDuplicate,
   validAnagram,
 };
